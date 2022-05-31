b685e74e14b264740eb560a951e301206d617c11
Justin Yoo <justin.yoojh@gmail.com>
2022-05-31 13:23:34 +0900

    Facade App (#1)

---
+++ FacadeApp/Models/Metadata.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+
+namespace FacadeApp.Models
+{
+    public class Metadata
+    {
+        public virtual Dictionary<string, string> Links { get; set; }
+
+        [JsonProperty("next_page")]
+        public virtual string NextPage { get; set; }
+
+        [JsonProperty("wpcom")]
+        public virtual bool IsWordpressCom { get; set; }
+    }
+}
